45c984cf111c550a5e89368e51d73c6b0a6e6936
unknown <gustavo.rodriguesssbr@gmail.com>
2024-02-22 15:46:17 -0300

    Classe ControleUsuario

--- lojaVirtual/src/controller/ControleUsuario.java
+++ lojaVirtual/src/controller/ControleUsuario.java
@@ -1,4 +1,30 @@
 package controller;
 
+import model.Usuario;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
 public class ControleUsuario {
+    private static final String REGEX_SENHA = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$";
+    public List<Usuario> listaDeUsuarios = new ArrayList<>();
+
+    public boolean validarSenha(String senha) {
+        Pattern pattern = Pattern.compile(REGEX_SENHA);
+        return pattern.matcher(senha).matches();
+    }
+
+    public boolean verificarEmail(String email) {
+        for (Usuario usuario : this.listaDeUsuarios) {
+            if (usuario.getEmail().equals(email)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void cadastrarUsuario(Usuario usuario) {
+        this.listaDeUsuarios.add(usuario);
+    }
 }
